de9551d9ae0e8539f2098cfbc4c2688e4ad05562
Bryan Barnes <bryan.barnes12@gmail.com>
2021-06-02 22:49:03 -0500

    adding beginning question and switch

--- index.js
+++ index.js
@@ -9,7 +9,43 @@ const connection = mysql.createConnection({
   database: 'EmpManSys_DB',
 });
 
+function manageOrg() {
+    inquirer
+    .prompt({
+      name: 'action',
+      type: 'list',
+      message: 'Select an action...',
+      choices: ['Add employee', 'Add department', 'Add role', 'Quit'],
+    })
+    // .then((action) => {
+    //     if (action === 'Quit') {
+    //         console.log('Goodbye');
+    //         connection.end();
+    //     }
+    // });
+    .then((action) => {
+        switch (action.action) {
+            case 'Add employee':
+                console.log('employee added');
+                connection.end();
+                break;
+            case 'Add department':
+                console.log('department added');
+                connection.end();
+                break;
+            case 'Add role':
+                console.log('role added');
+                connection.end();
+                break;
+            case 'Quit':
+                console.log('Goodbye');
+                connection.end();
+                break;
+        }
+    });
+}
+
 connection.connect((err) => {
   if (err) throw err;
-  connection.end();
+  manageOrg();
 });
